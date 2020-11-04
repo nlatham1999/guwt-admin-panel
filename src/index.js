407c948f2e2fb2caa8c189269f642d2dc4f4b1e0
nick Latham <nlatham@zagmail.gonzaga.edu>
2020-11-04 14:30:20 -0800

    git: updateing my fork

---
+++ src/index.js
@@ -0,0 +1,67 @@
+<<<<<<< HEAD
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
+import * as serviceWorker from './serviceWorker';
+// or
+// import { GoogleLogin } from 'react-google-login';
+ 
+ 
+const responseGoogle = (response) => {
+  console.log(response);
+}
+ 
+ReactDOM.render(
+  <GoogleLogin
+    clientId="240861169169-t9cdvoib0j4qjtoob8rad02vo94vetut.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />,
+  document.getElementById('googleButton')
+);
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import LoginForm from './LoginForm'
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <LoginForm />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+=======
+// src/index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+
+import "./index.css";
+
+ReactDOM.render(
+  <Router>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </Router>,
+  document.getElementById("root")
+);
+
+>>>>>>> upstream/master
