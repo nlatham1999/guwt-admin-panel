74aae6faffbd3f49c505540117f23eb3f3a54840
nick Latham <nlatham@zagmail.gonzaga.edu>
2020-12-02 10:59:25 -0800

    feat: added the dropdown to choose an organization

--- src/views/selectOrganizationView.js
+++ src/views/selectOrganizationView.js
@@ -18,7 +18,7 @@ const SelectOrganization = () => {
 
     if(organizationChosen){
         return (
-            <OrganizationMainPage setOrgChosen = {setOrganizationChosen}/>
+            <OrganizationMainPage setOrgChosen={setOrganizationChosen} organizationName={organizationName}/>
         );
     }
     return (
@@ -28,12 +28,19 @@ const SelectOrganization = () => {
             <ul>
                 <li>connect to the aws and get a list of organizations</li>
                 <li>create a dropdown or a list with tours</li>
-                <li>if a tour is selected, check to make sure that the user has access</li>
+                <li>if a organization is selected, check to make sure that the user has access</li>
                 <li> if not then as if they want to request access</li>
                 <li>if so then go to the organziation main page</li>
                 <li>add a create organization button</li>
             </ul>
-            <button onClick={() => goToOrganization()}>Temporary button to go to the organization main page</button>
+
+            {/* display the dropdown for an organization */}
+            <ViewDropdown setOrganizationChosen={setOrganizationChosen} setOrganizationName={setOrganizationName} />
+            
+            {/* if an organization has been chosen then run goToOrganization() */}
+            {organizationChosen == true && goToOrganization()}
+
+            {/* display the logout button */}
             <LogoutButton />
         </div>
     );
@@ -43,5 +50,48 @@ const SelectOrganization = () => {
         setOrganizationChosen(true);
     }
 }
+
+//dropdown button to choose an organization
+const ViewDropdown = ({ setOrganizationChosen, setOrganizationName}) => {
+
+    const [open, setOpen] = React.useState(false);
+    const drop = React.useRef(null);
+    const data = ["Gonzaga History Department", "Spokane Historical Scoiety"];
+  
+    function handleClick(e) {
+      if (!e.target.closest(`.${drop.current.className}`) && open) {
+        setOpen(false);
+      }
+    }
+  
+    function handleSelection(selection) {
+      setOrganizationName(selection);
+      setOrganizationChosen(true)
+      setOpen(false);
+    }
+  
+    React.useEffect(() => {
+      document.addEventListener("click", handleClick);
+      return () => {
+        document.removeEventListener("click", handleClick);
+      };
+    });
+  
+    return (
+      <div className="dropdown" ref={drop} >
+        <button onClick={() => setOpen(open => !open)}>Select an Organization</button>
+        {
+          open &&
+          <ul>
+            {data.map((item, i) => (
+              <li key={i} onClick={() => handleSelection(item)}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        }
+      </div>
+    );
+  };
 
 export default SelectOrganization;
