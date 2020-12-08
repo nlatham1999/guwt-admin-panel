134538c4b3100b9d96bf33bdecae8cb762bcd243
Michael Peters <michaelpeterswa@icloud.com>
2020-12-07 17:29:32 -0800

    fix: no-unused-vars hotfix

--- src/views/homeView.js
+++ src/views/homeView.js
@@ -7,8 +7,6 @@
 //there should also be a logout button at the topa
 
 import React, { useState } from "react";
-
-import { useAuth0 } from "@auth0/auth0-react";
 import Button from "react-bootstrap/Button";
 
 
