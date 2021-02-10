7756c506a07be2cf8c35b5a043a61b4998d2ab31
nick Latham <nlatham@zagmail.gonzaga.edu>
2021-02-10 11:39:23 -0800

    feat: cleaned up files, added documentation, added uploading files in the media panel

--- src/views/media-view.js
+++ src/views/media-view.js
@@ -11,28 +11,45 @@ import ImageUploader from 'react-images-upload';
 const MediaView = () => {
     const randomTestText = "Images Available";
     const [isClicked, setisClicked] = useState(false);
+    const [selectedFile, setSelectedFile] = useState(null)
     // const [open, setOpen] = React.useState(false);
+
+    const onFileChange = (event) => {
+    
+        setSelectedFile(event.target.files[0])
+      
+    };
 
     return (
         <div>
             <>
-            <input type="file">upload</input>
+            <input type="file" onChange={onFileChange}/>
             </>
             <Button onClick={() => displayMedia()}>Display Available Media</Button>
             {
+                selectedFile &&
+                <div>
+                    {selectedFile.type}
+
+                </div>
+            }
+            {
                 isClicked === true &&
                 <div>
-                <h1>
-                    {randomTestText}
-                </h1>
+                    <h1>
+                        {randomTestText}
+                    </h1>
                 {/* Display the image in the images folder
                 *IMPORTANT* require() can be used for .mp3, .mp4, .mov, .pdf etc
                 */}
-                <img src={pic} height="300" width="300" alt="Cannot Display"></img>
+                <img src={URL.createObjectURL(selectedFile)} height="300" width="300" alt="Cannot Display"></img>
+                     
                 </div>
             }
         </div>
     );
+
+
 
 
     function displayMedia(){
